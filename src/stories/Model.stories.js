a3e00256ea62559edf71ce12e46d170b54599d55
Ramona Harrison <matthewlisterb@gmail.com>
2021-03-03 23:23:07 +0100

    added body slot and italicised slot

--- src/stories/Model.stories.js
+++ src/stories/Model.stories.js
@@ -1,4 +1,6 @@
 import Model from '../components/Model.svelte';
+import ModelWithShortBody from './ModelWithShortBody.svelte';
+import ModelWithLongBody from './ModelWithLongBody.svelte';
 
 export default {
 	title: 'Model',
@@ -6,7 +8,7 @@ export default {
 };
 
 const Template = args => ({
-	Component: Model,
+	Component: ModelWithLongBody,
 	props: args,
 });
 
@@ -16,15 +18,17 @@ Default.args = {
 	name: 'model1',
 	title: 'lorem Ipsum',
 	subtitle: 'Lorem Ipsum',
-	body:
-		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec urna turpis, dictum ut tempus eget, sollicitudin ac erat. Sed ut pellentesque neque. Proin at erat vel tortor imperdiet finibus ut vitae dui. Suspendisse tristique nisl ut odio ultrices, iaculis venenatis nisi congue. Nam bibendum vitae sem eu porta. Aenean vehicula mauris quis eros condimentum, at iaculis orci dictum. Fusce efficitur quam nisl, nec varius ante placerat quis. Donec venenatis, orci quis faucibus rhoncus, leo nisi venenatis nunc, sed posuere urna libero vitae tellus. Duis tortor metus, accumsan ullamcorper scelerisque ut, efficitur nec mauris. Nunc dapibus massa id pulvinar finibus. Praesent justo ligula, tincidunt a mauris ut, porttitor commodo turpis.',
 };
 
-export const ShortBody = Template.bind({});
+const Template2 = args => ({
+	Component: ModelWithShortBody,
+	props: args,
+});
+
+export const ShortBody = Template2.bind({});
 ShortBody.args = {
 	imageSrc: 'https://images-na.ssl-images-amazon.com/images/I/41dE4XUrKSL._AC_.jpg',
 	name: 'model1',
 	title: 'lorem Ipsum',
 	subtitle: 'Lorem Ipsum',
-	body: 'Lorem ipsum dolor sit amet',
 };
---
+++ src/stories/ModelWithShortBody.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Model from '../components/Model.svelte';
+
+    export let imageSrc = '';
+	export let name = '';
+	export let title = '';
+	export let subtitle = '';
+</script>
+
+<Model {imageSrc} {name} {title} {subtitle}>
+    <span slot="italicised">2017-1019</span>
+    <span slot="body">Lorem ipsum dolor sit amet</span>
+</Model>
