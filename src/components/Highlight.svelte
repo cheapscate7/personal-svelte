25509a499f781c0a208127ca578c663e3fce03b5
Ramona Harrison <matthewlisterb@gmail.com>
2021-03-05 23:00:14 +0100

    added highlight component

--- src/components/Highlight.svelte
+++ src/components/Highlight.svelte
@@ -1,16 +1,44 @@
 <script lang="typescript">
-    export let title: string = "";
-    export let imageSrc: string = "";
+	import HighlightList from './HighlightList.svelte';
 
-    const defaultImage = '/image-not-found.png';
+	export let title: string = '';
+	export let imageSrc: string = '';
+	export let highlightTitle: string = '';
+	export let highlightList: HighlightListItem[] = [];
+
+	const defaultImage = '/image-not-found.png';
 </script>
 
+<style lang="less">
+	.container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		padding: 16px 24px;
+		box-sizing: border-box;
+		background-color: var(--theme-light-colours-alternate);
+		border-radius: 3px;
+		box-shadow: var(--theme-light-boxshadows-1);
+
+		.text-container {
+			width: (7/12 * 100%);
+		}
+
+		img {
+			// width: (5/12 * 100%);
+			box-shadow: var(--theme-light-boxshadows-1);
+			max-height: 480px;
+			object-fit: contain;
+		}
+	}
+</style>
+
 <div class="container">
-    <div class="text-container">
-        <h2>{title}</h2>
-        <slot name="body" />
-    </div>
-    <div class="media-container">
-        <img src={imageSrc || defaultImage} alt={`Highlight ${title} - Image`} />
-    </div>
+	<div class="text-container">
+		<h2>{title}</h2>
+		<slot name="body" />
+		<HighlightList title={highlightTitle} highlights={highlightList} />
+	</div>
+	<img src={imageSrc || defaultImage} alt={`Highlight ${title} - Image`} />
 </div>
--- src/stories/Highlight.stories.js
+++ src/stories/Highlight.stories.js
@@ -1,4 +1,5 @@
 import HighlightWithBody from './HighlightWithBody.svelte';
+import MockImage from '../../public/IMG_20200923_182129.jpg';
 
 export default {
 	title: 'Highlight',
@@ -13,5 +14,14 @@ const Template = args => ({
 export const Default = Template.bind({});
 Default.args = {
 	title: 'Who am I?',
-    imageSrc: "/public/IMG_20200923_182129.jpg"
+	imageSrc: MockImage,
+	highlightList: [
+		{ label: 'Smart' },
+		{ label: 'Witty' },
+		{
+			label:
+				'Talented if i were to go on for a long time, this would get really long. lets see what happens to the list item if i do that. wow this is going on for a long time',
+		},
+	],
+	highlightTitle: 'Why am I so cool?',
 };
